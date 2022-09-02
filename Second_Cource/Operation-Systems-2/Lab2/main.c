a512a24ea48c497fb33f845a8f0c46284dff3740
plugatarev <plugatarevk@gmail.com>
2022-09-02 17:11:55 +0700

    Lab2

---
+++ Second_Cource/Operation-Systems-2/Lab2/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+
+#define SUCCESS 0
+
+void* threadFunction(void* param){
+    for (int i = 0; i < 10; i++) {
+        printf("hello child %d\n", i);
+    }
+}
+
+int main(int argc, char** argv) {
+    pthread_t thread;
+    int error = pthread_create(&thread, NULL, threadFunction, NULL);
+    if (error != SUCCESS){
+        perror("pthread_create");
+    }
+
+    pthread_join(thread, NULL);
+
+    for (int i = 0; i < 10; i++) {
+        printf("hello parent %d\n", i);
+    }
+
+    pthread_exit(NULL);
+}
