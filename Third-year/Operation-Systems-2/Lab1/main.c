e04300b9b852acacb8732426805edf0a8c7192e5
plugatarev <plugatarevk@gmail.com>
2022-09-05 11:46:45 +0700

    handling errors

--- Third-year/Operation-Systems-2/Lab1/main.c
+++ Third-year/Operation-Systems-2/Lab1/main.c
@@ -3,11 +3,17 @@
 #include <string.h>
 
 #define SUCCESS 0
+#define LINES_COUNT 10
 
-void* threadFunction(void* param){
-    for (int i = 0; i < 10; i++) {
+void* printLines(void* param){
+    if (param == NULL) {
+        printf("Unexpected argument value: NULL");
+        return NULL;
+    }
+    for (int i = 0; i < LINES_COUNT; i++) {
         printf("hello child %d\n", i);
     }
+    return NULL;
 }
 
 int main(int argc, char** argv) {
@@ -22,4 +28,5 @@ int main(int argc, char** argv) {
     }
 
     pthread_exit(NULL);
+    return SUCCESS;
 }
