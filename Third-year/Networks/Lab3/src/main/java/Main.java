6f157d73688b0b93eef45491e3525fb7cb6715c6
plugatarev <plugatarevk@gmail.com>
2022-10-17 13:53:18 +0700

    main

--- Third-year/Networks/Lab3/src/main/java/Main.java
+++ Third-year/Networks/Lab3/src/main/java/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+import controller.ApplicationController;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationController controller = new ApplicationController();
+        controller.run();
+    }
 }
