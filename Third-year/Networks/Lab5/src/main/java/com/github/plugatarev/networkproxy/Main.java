2bd232c77e2f3ef641d0ca26bc87de500e42323e
plugatarev <plugatarevk@gmail.com>
2022-11-02 12:03:08 +0700

    Main

--- Third-year/Networks/Lab5/src/main/java/com/github/plugatarev/networkproxy/Main.java
+++ Third-year/Networks/Lab5/src/main/java/com/github/plugatarev/networkproxy/Main.java
@@ -1,12 +1,13 @@
 package com.github.plugatarev.networkproxy;
 
+import com.github.plugatarev.networkproxy.proxy.Proxy;
 import org.apache.log4j.Logger;
 
 public final class Main {
     private static final Logger logger = Logger.getLogger(Main.class);
 
-    private static final int MIN_PORT = 0;
-    private static final int MAX_PORT = 0xFFFF;
+    private static final int MIN_PORT = 1025;
+    private static final int MAX_PORT = 65535;
 
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -23,7 +24,7 @@ public final class Main {
             }
         }
         catch (NumberFormatException exception) {
-            logger.error("Unable to parse port {" + args[0] + "} ", exception);
+            logger.error("Incorrect port format ", exception);
             return;
         }
 
